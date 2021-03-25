e953ff027b7341f6214084aab4503147ab157ef8
euberdeveloper <euberdeveloper@gmail.com>
2021-03-25 21:58:10 +0100

    fix(tabellone.job): fix cron when changed
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/jobs/tabellone.job.ts
+++ source/jobs/tabellone.job.ts
@@ -8,14 +8,19 @@ export const JOB_NAME = 'tabellone';
 export function loadTabellone(bull: Queue): void {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     bull.process(JOB_NAME, async () => {
-        const recipients = await tabelloneService.getRecipients();
-        const { filePath, fileName } = await tabelloneService.storeTabelloneTsv();
-        await emailService.tabellone(recipients, filePath, fileName);
+        //const recipients = await tabelloneService.getRecipients();
+        // const { filePath, fileName } = await tabelloneService.storeTabelloneTsv();
+        // await emailService.tabellone(/*recipients*/ ['euberdeveloper@gmail.com'], filePath, fileName);
+        console.log('ciao');
     });
 }
 
 export async function addTabellone(bull: Queue): Promise<void> {
     const cron = CONFIG.JOBS.TABELLONE_CRON;
-    await bull.removeRepeatableByKey(JOB_NAME);
+    const oldJobsKeys = (await bull.getRepeatableJobs()).filter(j => j.name === JOB_NAME).map(j => j.key);
+    for (const oldJobKey of oldJobsKeys) {
+        await bull.removeRepeatableByKey(oldJobKey);
+    }
+
     await bull.add(JOB_NAME, null, { repeat: { cron } });
 }
