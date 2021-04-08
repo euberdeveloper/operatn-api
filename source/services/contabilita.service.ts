87e540976e55bd937002e18ac1b724e98c06e3d8
euberdeveloper <euberdeveloper@gmail.com>
2021-04-08 22:20:17 +0200

    feat(contabilita): continue contabilita
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/services/contabilita.service.ts
+++ source/services/contabilita.service.ts
@@ -1,21 +1,22 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import axios from 'axios';
 import prisma, { Bolletta, Quietanziante, TipoBolletta } from '@/services/prisma.service';
-import { InvalidQueryParamError } from '@/errors';
+import { ContabilitaError, InvalidQueryParamError } from '@/errors';
+import logger from '@/utils/logger';
 import CONFIG from '@/config';
 
-type RawResult = Bolletta & {
+type BollettaInfo = Bolletta & {
     quietanziante: Quietanziante;
     tipoBolletta: TipoBolletta;
 };
 
 export class ContabilitaService {
-    private async fetchBollette(start: Date, end: Date): Promise<RawResult[]> {
+    private async fetchBollette(start: Date, end: Date): Promise<BollettaInfo[]> {
         return prisma.bolletta.findMany({
             where: {
                 dataInvioEusis: null,
                 NOT: {
-                    OR: [{ competenzaAl: { lte: start } }, { competenzaDal: { gte: end } }]
+                    OR: [{ dataScadenza: { lte: start } }, { dataScadenza: { gte: end } }]
                 }
             },
             include: {
@@ -25,7 +26,7 @@ export class ContabilitaService {
         });
     }
 
-    private getXmlFromBollette(bollette: RawResult[]): string {
+    private getXmlFromBolletta(bolletta: BollettaInfo): string {
         return `
         <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
         <soapenv:Header></soapenv:Header>
@@ -61,64 +62,54 @@ export class ContabilitaService {
                     <tem:descr>LM WO  - B - 2021/417 RIFER.MESE : 5 - ANNO : 2021</tem:descr>
                     <tem:numdoc_def>B-374787</tem:numdoc_def>
                 </tem:testata>
-                ${bollette
-                    .map(
-                        b => `
-                            <tem:riga>
-                                <tem:riga>1</tem:riga>
-                                <tem:tipo>T</tem:tipo>
-                                <tem:codtar>1</tem:codtar>
-                                <tem:prefisso>1</tem:prefisso>
-                                <tem:codice>011</tem:codice>
-                                <tem:descr>${b.tipoBolletta.tipoBolletta}</tem:descr>
-                                <tem:qta>1</tem:qta>
-                                <tem:prezzo>${b.importoTotale}</tem:prezzo>
-                                <tem:codiva>110</tem:codiva>
-                                <tem:capitolo></tem:capitolo>
-                                <tem:siglacapitolo>031002020</tem:siglacapitolo>
-                                <tem:conto></tem:conto>
-                                <tem:siglaconto>031002020</tem:siglaconto>
-                                <tem:importo>${b.importoTotale}</tem:importo>
-                                <tem:desc_art>${b.tipoBolletta.tipoBolletta}</tem:desc_art>
-                                <tem:dtcompini>2021-05-01</tem:dtcompini>
-                                <tem:dtcompfin>2021-05-01</tem:dtcompfin>
-                                <tem:fatcdc>
-                                    <tem:conto></tem:conto>
-                                    <tem:siglaconto>031002020</tem:siglaconto>
-                                    <tem:progr>1</tem:progr>
-                                    <tem:tipocod>R</tem:tipocod>
-                                    <tem:codcdc>112010201</tem:codcdc>
-                                    <tem:fat_prod></tem:fat_prod>
-                                    <tem:aa1>2021</tem:aa1>
-                                    <tem:mm1>5</tem:mm1>
-                                    <tem:aa2>2021</tem:aa2>
-                                    <tem:mm2>5</tem:mm2>
-                                    <tem:importo>${b.importoTotale}</tem:importo>
-                                </tem:fatcdc>
-                            </tem:riga>
-                        `
-                    )
-                    .join('\n')}
-                    ${bollette
-                        .map(
-                            b => `
-                            <tem:fatscad>
-                                <tem:capitolo></tem:capitolo>
-                                <tem:siglacapitolo>031002020</tem:siglacapitolo>
-                                <tem:annoacc>2021</tem:annoacc>
-                                <tem:accertamento>2</tem:accertamento>
-                                <tem:subacc>0</tem:subacc>
-                                <tem:scadenza>${b.dataScadenza.toISOString().slice(0, 10)}</tem:scadenza>
-                                <tem:importo>${b.importoTotale}</tem:importo>
-                                <tem:dtcompini>2021-05-01</tem:dtcompini>
-                                <tem:dtcompfin>2021-05-01</tem:dtcompfin>
-                            </tem:fatscad>
-                            `
-                        )
-                        .join('\n')}
+            
+                <tem:riga>
+                    <tem:riga>1</tem:riga>
+                    <tem:tipo>T</tem:tipo>
+                    <tem:codtar>1</tem:codtar>
+                    <tem:prefisso>1</tem:prefisso>
+                    <tem:codice>011</tem:codice>
+                    <tem:descr>${bolletta.tipoBolletta.tipoBolletta}</tem:descr>
+                    <tem:qta>1</tem:qta>
+                    <tem:prezzo>${bolletta.importoTotale}</tem:prezzo>
+                    <tem:codiva>110</tem:codiva>
+                    <tem:capitolo></tem:capitolo>
+                    <tem:siglacapitolo>031002020</tem:siglacapitolo>
+                    <tem:conto></tem:conto>
+                    <tem:siglaconto>031002020</tem:siglaconto>
+                    <tem:importo>${bolletta.importoTotale}</tem:importo>
+                    <tem:desc_art>${bolletta.tipoBolletta.tipoBolletta}</tem:desc_art>
+                    <tem:dtcompini>2021-05-01</tem:dtcompini>
+                    <tem:dtcompfin>2021-05-01</tem:dtcompfin>
+                    <tem:fatcdc>
+                        <tem:conto></tem:conto>
+                        <tem:siglaconto>031002020</tem:siglaconto>
+                        <tem:progr>1</tem:progr>
+                        <tem:tipocod>R</tem:tipocod>
+                        <tem:codcdc>112010201</tem:codcdc>
+                        <tem:fat_prod></tem:fat_prod>
+                        <tem:aa1>2021</tem:aa1>
+                        <tem:mm1>5</tem:mm1>
+                        <tem:aa2>2021</tem:aa2>
+                        <tem:mm2>5</tem:mm2>
+                        <tem:importo>${bolletta.importoTotale}</tem:importo>
+                    </tem:fatcdc>
+                </tem:riga>
+        
+                <tem:fatscad>
+                    <tem:capitolo></tem:capitolo>
+                    <tem:siglacapitolo>031002020</tem:siglacapitolo>
+                    <tem:annoacc>2021</tem:annoacc>
+                    <tem:accertamento>2</tem:accertamento>
+                    <tem:subacc>0</tem:subacc>
+                    <tem:scadenza>${bolletta.dataScadenza.toISOString().slice(0, 10)}</tem:scadenza>
+                    <tem:importo>${bolletta.importoTotale}</tem:importo>
+                    <tem:dtcompini>2021-05-01</tem:dtcompini>
+                    <tem:dtcompfin>2021-05-01</tem:dtcompfin>
+                </tem:fatscad>
             </tem:InsCarFatRequest>
         </soapenv:Body>
-    </soapenv:Envelope>
+        </soapenv:Envelope>
         `;
     }
 
@@ -145,12 +136,39 @@ export class ContabilitaService {
     public async sendBollette(queryParams: any): Promise<void> {
         const [startDate, endDate] = this.extractDatesFromQuery(queryParams);
         const bollette = await this.fetchBollette(startDate, endDate);
-        const soapRequest = this.getXmlFromBollette(bollette);
-        await axios.post(CONFIG.CONTABILITA.API_URL, soapRequest, {
-            headers: {
-                'Content-Type': 'application/xml'
+
+        const failedBollette: number[] = [];
+        for (const bolletta of bollette) {
+            const soapRequest = this.getXmlFromBolletta(bolletta);
+            try {
+                await prisma.bolletta.update({ where: { id: bolletta.id }, data: { dataInvioEusis: new Date() } });
+                await axios.post(CONFIG.CONTABILITA.API_URL, soapRequest, {
+                    headers: {
+                        'Content-Type': 'application/xml'
+                    }
+                });
+            } catch (error) {
+                let err;
+                if (error.response) {
+                    err = { status: error.response.status, data: error.response.data };
+                } else if (error.request) {
+                    err = error.message;
+                } else {
+                    err = error;
+                }
+                try {
+                    await prisma.bolletta.update({ where: { id: bolletta.id }, data: { dataInvioEusis: new Date() } });
+                } catch (error) {
+                    logger.warning('ERROR, cannot reset data invio EUSIS to null of bolletta with id', bolletta.id);
+                }
+                logger.warning('Error in contabilità', err);
+                failedBollette.push(bolletta.id);
             }
-        });
+        }
+
+        if (failedBollette.length) {
+            throw new ContabilitaError(undefined, failedBollette);
+        }
     }
 }
 
