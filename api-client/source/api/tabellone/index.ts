60161ee815f6d443f31e95020a49cade2c8869cb
euberdeveloper <euberdeveloper@gmail.com>
2021-04-20 22:06:22 +0200

    fix(api-client): add tipo to tabellone
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- api-client/source/api/tabellone/index.ts
+++ api-client/source/api/tabellone/index.ts
@@ -1,5 +1,39 @@
-import { Tabellone } from "@server/services/tabellone.service";
+import { Sesso } from "@/db-types";
 import { AxiosContainer, BaseController } from "@/utils/baseController";
+
+export interface Tabellone {
+    fabbricatoId: number;
+    fabbricatoCodice: string;
+    fabbricatoNome: string;
+    fabbricatoComune: string;
+    fabbricatoIndirizzo: string;
+    fabbricatoNCivico: string;
+    stanzaUnitaImmobiliare: string;
+    stanzaPiano: string | null;
+    tipoStanzaTipoStanza: string;
+    stanzaNumeroStanza: string;
+    postoLettoPostoLetto: string;
+    stanzaGestioneDiretta: boolean;
+    stanzaNote: string | null;
+    manutenzioneDataCreazione: string | null;
+    personaId: number | null;
+    personaNome: string | null;
+    personaCognome: string | null;
+    personaCodiceFiscale: string | null;
+    personaSesso: Sesso | null;
+    ospiteCittadinanza: string | null;
+    ospiteEmail: string | null;
+    ospiteTelefonoPrincipale: string | null;
+    ospiteTelefonoSecondario: string | null;
+    dipartimentoUnitnCodice: string | null;
+    dipartimentoUnitnNome: string | null;
+    contrattoId: number | null;
+    contrattoDataInizio: string | null;
+    contrattoDataFine: string | null;
+    tipoContrattoSigla: string | null;
+    tipoOspiteSigla: string | null;
+    contrattoNote: string | null;
+}
 
 export interface TabelloneQueryParams {
     startDate: Date;
