edd3fd4cf7836a9f667f9eafad88a68b6da2e58f
euberdeveloper <euberdeveloper@gmail.com>
2021-04-20 15:14:56 +0200

    feat(api-client): add support for query params
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- api-client/source/utils/baseController.ts
+++ api-client/source/utils/baseController.ts
@@ -6,11 +6,18 @@ export interface AxiosContainer {
 
 export abstract class BaseController {
 
-     protected abstract ROUTE: string;
+    protected abstract ROUTE: string;
 
-    constructor(protected axiosContainer: AxiosContainer) {}
+    constructor(protected axiosContainer: AxiosContainer) { }
 
     protected get axiosInstance(): AxiosInstance {
         return this.axiosContainer.axiosInstance;
     }
+
+    protected parseQueryParams(params: any, parents: string[] = []): string {
+        const keys = Object.keys(params);
+        const prefix = parents.length ? parents.join() + '.' : '';
+        return keys
+          .reduce((result, key) => result + (typeof params[key] === 'object' ? this.parseQueryParams(params[key], [...parents, key]) : `${prefix}${key}=${params[key]}`), keys.length && !parents.length ? '?' : '');
+      }
 }
