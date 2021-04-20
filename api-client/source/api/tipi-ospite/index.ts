b850401f90084d312f3d12822aa6afbc78a6fcf0
euberdeveloper <euberdeveloper@gmail.com>
2021-04-20 15:15:05 +0200

    feat(api-client): add tipi ospite
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- api-client/source/api/tipi-ospite/index.ts
+++ api-client/source/api/tipi-ospite/index.ts
@@ -4,22 +4,30 @@ import { AxiosContainer, BaseController } from "@/utils/baseController";
 type TipiOspiteCreateBody = TipoOspite & { id?: number };
 type TipiOspiteReplaceBody = Omit<TipoOspite, 'id'>;
 
+export interface TipiOspiteIncludeParams {
+    contoRicaviConsumi?: boolean;
+    contoRicaviCanoni?: boolean;
+}
+
 export class TipiOspiteController extends BaseController {
 
     public ROUTE = '/tipi-ospite';
 
-    async getAll(): Promise<TipoOspite[]> {
-        const result = await this.axiosInstance.get(`${this.ROUTE}`);
+    async getAll(params: TipiOspiteIncludeParams = {}): Promise<TipoOspite[]> {
+        const queryParams = this.parseQueryParams(params);
+        const result = await this.axiosInstance.get(`${this.ROUTE}${queryParams}`);
         return result.data;
     }
 
-    async get(id: number): Promise<TipoOspite> {
-        const result = await this.axiosInstance.get(`${this.ROUTE}/${id}`);
+    async get(id: number, params: TipiOspiteIncludeParams = {}): Promise<TipoOspite> {
+        const queryParams = this.parseQueryParams(params);
+        const result = await this.axiosInstance.get(`${this.ROUTE}/${id}${queryParams}`);
         return result.data;
     }
 
-    async getByValue(sigla: string): Promise<TipoOspite> {
-        const result = await this.axiosInstance.get(`${this.ROUTE}/sigla/${sigla}`);
+    async getByValue(sigla: string, params: TipiOspiteIncludeParams = {}): Promise<TipoOspite> {
+        const queryParams = this.parseQueryParams(params);
+        const result = await this.axiosInstance.get(`${this.ROUTE}/sigla/${sigla}${queryParams}`);
         return result.data;
     }
 
