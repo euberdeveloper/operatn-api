866ec896eb7ec9fa8fe152d3346ee9a39ccf19ad
euberdeveloper <euberdeveloper@gmail.com>
2021-05-14 21:21:34 +0200

    feat(api-client): fix contabilita
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- api-client/source/api/controllers/contabilita/index.ts
+++ api-client/source/api/controllers/contabilita/index.ts
@@ -1,9 +1,39 @@
+import {
+    Ospite,
+    Persona,
+    LuogoDiNascita,
+    Residenza,
+    Bolletta,
+    Contratto,
+    Quietanziante,
+    TipoBolletta,
+    ContoRicavi
+} from '@/db-types';
 import { AxiosContainer, BaseController } from '@/utils/baseController';
 
 export interface ContabilitaQueryParams {
     startDate: Date;
     endDate: Date;
+    idTipoOspite?: number;
+    idOspite?: number;
+    siglaCausale?: string;
 }
+
+type OspiteInfo = Ospite & {
+    persona: Persona & {
+        luogoDiNascita: LuogoDiNascita | null;
+        residenza: Residenza | null;
+    };
+};
+
+export type ContabilitaBollettaInfo = Bolletta & {
+    contratto: Contratto;
+    quietanziante: Quietanziante;
+    ospite: OspiteInfo | null;
+    tipoBolletta: TipoBolletta;
+    contoRicaviCanoni: ContoRicavi | null;
+    contoRicaviConsumi: ContoRicavi | null;
+};
 
 export class ContabilitaController extends BaseController {
     public route = '/contabilita';
@@ -12,9 +42,18 @@ export class ContabilitaController extends BaseController {
         super(axiosContainer);
     }
 
-    public async sendBollette(params: ContabilitaQueryParams, options: Record<string, any> = {}): Promise<void> {
+    public async getBollette(
+        params: ContabilitaQueryParams,
+        options: Record<string, any> = {}
+    ): Promise<ContabilitaBollettaInfo[]> {
         const queryParams = this.parseQueryParams(params);
-        const result = await this.axiosInstance.get(`${this.route}${queryParams}`, { ...options });
+        const result = await this.axiosInstance.get(`${this.route}/bollette${queryParams}`, { ...options });
+        return result.data;
+    }
+
+    public async sendBollette(params: ContabilitaQueryParams, options: Record<string, any> = {}): Promise<number[]> {
+        const queryParams = this.parseQueryParams(params);
+        const result = await this.axiosInstance.post(`${this.route}/bollette${queryParams}`, { ...options });
         return result.data;
     }
 }
